0fc195b08283edc67f705e8f0a27933ece9db591
MelBiggs <melissabiggs16@gmail.com>
2020-06-03 11:12:28 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+# create an app instance
+
+# Home Page 
+@app.route('/')
+def home():
+    """ Main Page"""
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # on running python app.py
+    app.run(debug=True)    
