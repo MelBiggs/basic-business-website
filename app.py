13b03c99db6b53cc998bd03f0db0fa319695c876
MelBiggs <melissabiggs16@gmail.com>
2020-06-04 12:35:23 +0000

    Added bootstrap template

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # create an app instance
 
-# Home Page 
+# Home Page
+
+
 @app.route('/')
 def home():
     """ Main Page"""
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     # on running python app.py
-    app.run(debug=True)    
+    app.run(debug=True)
